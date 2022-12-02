31fb42df4b68656c79b31f9f465cfb01c4da5f13
DimCore3 <3dmi3y@gmail.com>
2022-12-02 17:08:58 +0400

    поправил верстку для изображений стилей, осталось подтягивать туда данные из входящего JSON

--- src/components/Content/AllStyleItems/AllStyleItems.vue
+++ src/components/Content/AllStyleItems/AllStyleItems.vue
@@ -2,15 +2,90 @@
     <div class="all-content-boxes">
         <a class="show-all-styles" href="">Посмотреть все стили</a>
         <div class="all-img-style">
-            <ImageBoxVue/>
+            <!-- <ImageBoxVue/> -->
+            <div class="large-img-test">
+                <div class="img-hover-info">
+                    <div class="take-copy img-hover-icons">
+                        <img src="/src/assets/copy.svg" alt="">
+                    </div>
+                    <div>
+                        <img class="img-hover-icons-bag" src="/src/assets/bag.svg" alt="">
+                        <p>Узнай что на мне</p>
+                    </div>
+                    <div>
+                        <img class="img-hover-icons" src="/src/assets/like.png" alt="">
+                        <p>200</p>
+                    </div>
+                </div>
+            </div>
             <div class="small-boxes-style">
                 <div class="img-column">
-                    <ImageBoxVue/>
-                    <ImageBoxVue/>
+                    <div class="small-img-test img-test-1">
+                        <div class="img-hover-info">
+                            <div class="take-copy img-hover-icons">
+                                <img src="/src/assets/copy.svg" alt="">
+                            </div>
+                            <div>
+                                <img class="img-hover-icons-bag" src="/src/assets/bag.svg" alt="">
+                                <p>Узнай что на мне</p>
+                            </div>
+                            <div>
+                                <img class="img-hover-icons" src="/src/assets/like.png" alt="">
+                                <p>200</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="small-img-test img-test-2">
+                        <div class="img-hover-info">
+                            <div class="take-copy img-hover-icons">
+                                <img src="/src/assets/copy.svg" alt="">
+                            </div>
+                            <div>
+                                <img class="img-hover-icons-bag" src="/src/assets/bag.svg" alt="">
+                                <p>Узнай что на мне</p>
+                            </div>
+                            <div>
+                                <img class="img-hover-icons" src="/src/assets/like.png" alt="">
+                                <p>200</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <ImageBoxVue/>
+                        <ImageBoxVue/> -->
                 </div>
                 <div class="img-column">
-                    <ImageBoxVue/>
-                    <ImageBoxVue/>
+                    <div class="small-img-test img-test-3">
+                        <div class="img-hover-info">
+                            <div class="take-copy img-hover-icons">
+                                <img src="/src/assets/copy.svg" alt="">
+                            </div>
+                            <div>
+                                <img class="img-hover-icons-bag" src="/src/assets/bag.svg" alt="">
+                                <p>Узнай что на мне</p>
+                            </div>
+                            <div>
+                                <img class="img-hover-icons" src="/src/assets/like.png" alt="">
+                                <p>200</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="small-img-test img-test-4">
+                        <div class="img-hover-info">
+                            <div class="take-copy img-hover-icons">
+                                <img src="/src/assets/copy.svg" alt="">
+                            </div>
+                            <div>
+                                <img class="img-hover-icons-bag" src="/src/assets/bag.svg" alt="">
+                                <p>Узнай что на мне</p>
+                            </div>
+                            <div>
+                                <img class="img-hover-icons" src="/src/assets/like.png" alt="">
+                                <p>200</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <ImageBoxVue/>
+                    <ImageBoxVue/> -->
                 </div>
             </div>
         </div>
@@ -28,8 +103,8 @@ export default defineComponent({
     props: {
         productData: Object,
     },
-    setup () {
-        
+    setup() {
+
 
         return {}
     }
@@ -37,19 +112,93 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-    .all-content-boxes {
-        display: flex;
-        flex-direction: column;
-        align-items:center;
-        width: 100%;
+.large-img-test {
+    background-image: url("https://github.com/DimCore3/alf-dev-test/blob/main/src/assets/139867.png?raw=true");
+    background-clip: border-box;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
 
-        .show-all-styles {
-            margin: 2rem 0 2rem 0;
-        }
-        .small-boxes-style {
-            display: flex;
-            width: 50%;
-        }
+.img-test-1 {
+    background-image: url("https://github.com/DimCore3/alf-dev-test/blob/main/src/assets/139867.png?raw=true");
+    background-clip: border-box;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+
+.img-test-2 {
+    background-image: url("https://github.com/DimCore3/alf-dev-test/blob/main/src/assets/139867.png?raw=true");
+    background-clip: border-box;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+
+.img-test-3 {
+    background-image: url("https://github.com/DimCore3/alf-dev-test/blob/main/src/assets/139867.png?raw=true");
+    background-clip: border-box;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+
+.img-test-4 {
+    background-image: url("https://github.com/DimCore3/alf-dev-test/blob/main/src/assets/139867.png?raw=true");
+    background-clip: border-box;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+
+.all-content-boxes {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+
+    .show-all-styles {
+        margin: 2rem 0 2rem 0;
     }
 
+    .small-boxes-style {
+        display: flex;
+    }
+}
+
+.img-hover-info {
+    display: none;
+}
+
+.img-hover-icons {
+    width: 17px;
+    height: 17px;
+}
+
+.img-hover-icons-bag {
+    width: 45px;
+    height: 45px;
+}
+
+.large-img-test:hover,
+.small-img-test:hover {
+    .img-hover-info {
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        text-align: center;
+        flex-direction: column;
+
+        .take-copy {
+            justify-content: flex-end;
+            margin: 1rem;
+        }
+
+        background-color: rgba($color: #000000, $alpha: 0.4);
+        color: white;
+    }
+}
 </style>
